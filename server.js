f47ede155bbdc3a0aac44855fd968b806668b22f
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-05-04 12:33:52 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+const loadRoute = require('./src/load');
+const uploadRoute = require('./src/upload');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use(loadRoute);
+
+app.use(uploadRoute);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
