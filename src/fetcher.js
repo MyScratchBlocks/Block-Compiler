12b0e1e0de4793f462983a2189ec26298a4ba73f
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-05-05 09:52:25 +0100

    Create fetcher.js

---
+++ src/fetcher.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const AdmZip = require('adm-zip');
+const fetch = require('node-fetch');
+
+const router = express.Router();
+
+const GITHUB_RAW_BASE = 'https://raw.githubusercontent.com/CodeSnap-ORG/Editor-Compiler/main/uploads';
+
+router.get('/:id', async (req, res) => {
+    const fileId = req.params.id;
+    const fileUrl = `${GITHUB_RAW_BASE}/${fileId}.sb3`;
+
+    try {
+        const response = await fetch(fileUrl);
+
+        if (!response.ok) {
+            return res.status(404).send('SB3 file not found on GitHub');
+        }
+
+        const buffer = await response.buffer();
+        const zip = new AdmZip(buffer);
+        const projectJsonEntry = zip.getEntry('project.json');
+
+        if (!projectJsonEntry) {
+            return res.status(400).send('project.json not found in SB3 file');
+        }
+
+        const projectJson = projectJsonEntry.getData().toString('utf8');
+        const projectData = JSON.parse(projectJson);
+
+        res.json(projectData);
+    } catch (err) {
+        console.error('Error processing SB3 file:', err);
+        res.status(500).send('Failed to fetch or process SB3 file');
+    }
+});
+
+module.exports = router;
