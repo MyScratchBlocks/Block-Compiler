5bbbd6a78ea2e47bd470943d0c1280e6ce7b36e9
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-05-26 09:56:01 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,11 +2,8 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const loadRoute = require('./load');
 const uploadRoute = require('./upload');
-const fetcherRoute = require('./fetcher');
 const projectsRoute = require('./projects');
-const commentRoute = require('./test');
 const clientRoute = require('./client');
 
 dotenv.config();
@@ -16,12 +13,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use(loadRoute);
-app.use(fetcherRoute);
 app.use(projectsRoute);
 app.use(uploadRoute);
 app.use(clientRoute);
-app.use('/api/comments', commentRoute);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
