66eb64323603b7ec68229a664cc23145b07eee30
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-06-23 16:49:14 +0100

    Update createProject.js

--- src/createProject.js
+++ src/createProject.js
@@ -27,7 +27,7 @@ async function githubApiRequest(method, url, data = null) {
     method,
     url: `https://api.github.com${url}`,
     headers: {
-      'Authorization': `Bearer ${GITHUB_TOKEN}`,
+      'Authorization': `token ${GITHUB_TOKEN}`,
       'Accept': 'application/vnd.github+json'
     },
     data
