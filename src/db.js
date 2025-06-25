1df4f015d264280be6bd89fd90dff6de3f24b09d
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-06-25 07:59:18 +0100

    Delete src/db.js

--- src/db.js
+++
@@ -1,8 +0,0 @@
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  connectionString: 'postgresql://neondb_owner:npg_zenEQCNp8J4K@ep-purple-paper-a8qvap9g-pooler.eastus2.azure.neon.tech/neondb?sslmode=require
-  ssl: { rejectUnauthorized: false } // Required by Neon
-});
-
-module.exports = pool;
