80f722803114ff35c01e1249e018a3408458f489
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-06-26 17:48:38 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -27,7 +27,7 @@ const usersRouter = require('./users');
 
 
 // app.use(require('./hackv2'));
-app.use(require('./upload'));
+// app.use(require('./upload'));
 app.use(commentsRouter); // Comments
 app.use(projectsRoute);  // Projects
 app.use(usersRouter);       // Users
