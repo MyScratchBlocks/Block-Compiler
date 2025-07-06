cbb9017f548b2b0fdeb9a2ec5867842c64a21aee
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-07-06 09:36:56 +0100

    Update messages.js

--- src/messages.js
+++ src/messages.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-let messages = [];
+let messages = {};
 
 router.get('/users/:user/messages', (req, res) => {
-  res.json({ messages[req.params.user] });
+  res.json({ messages: messages[req.params.user] || [] });
 });
 
 const addMessage = function (user, message) {
-  messages[user] += message;
- } 
+  if (!messages[user]) messages[user] = [];
+  messages[user].push(message);
+};
 
 module.exports = { addMessage, router };
